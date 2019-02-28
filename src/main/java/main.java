40a399750718889a1012ed9646a5946a1cdc24e7
Kunpeng Xie <kx1005@wildcats.unh.edu>
2019-02-27 20:10:54 -0500

    modify some code of UJM.java and UL.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -30,10 +30,10 @@ class Main {
 
 
 
-//        INDEX_DIRECTORY = args[0];
-//        queryPath = args[1];
-//        //dataPath = args[2];
-//        OUTPUT_DIR = args[2];
+        INDEX_DIRECTORY = args[0];
+        queryPath = args[1];
+        //dataPath = args[2];
+        OUTPUT_DIR = args[2];
 
         //indexer = new IndexData(INDEX_DIRECTORY, dataPath);
         QueryData queryData = new QueryData(queryPath);
