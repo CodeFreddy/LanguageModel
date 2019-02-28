2d9b460b6224c2212dd0b8e126ed6715b617f716
Kunpeng Xie <kx1005@wildcats.unh.edu>
2019-02-28 12:45:50 -0500

    run all the methods

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -57,24 +57,26 @@ class Main {
         System.out.println("================");
         System.out.println("length is: " + pageList.size());
 
-//
-//        UL page_ul = new UL(pageMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR,"UnigramLanguageModel-Laplace-Page.run");
-//        UL section_ul = new UL(sectionMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-Laplace-Section.run");
+        // UL
+        UL page_ul = new UL(pageMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR,"UnigramLanguageModel-Laplace-Page.run");
+        UL section_ul = new UL(sectionMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-Laplace-Section.run");
 
-//
-//        UDS page_uds = new UDS(pageMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-UDS-Page.run");
-//        UDS section_uds = new UDS(sectionMap,Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-UDS-Section.run");
+        // UDS
+        UDS page_uds = new UDS(pageMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-UDS-Page.run");
+        UDS section_uds = new UDS(sectionMap,Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-UDS-Section.run");
+
+        // UJM
         UJM page_ujm = new UJM(pageMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-UJM-Page.run");
         UJM section_ujm = new UJM(sectionMap, Max_Results, INDEX_DIRECTORY, OUTPUT_DIR, "UnigramLanguageModel-UJM-Section.run");
 
-//        writeFile("UnigramLanguageModel-JM-Section.run", section_ujm.getList());
+
 
         // BL
-//        System.out.println("Running Biagram Language Model with Laplace Smoothing...");
-//        BL page_bl = new BL(INDEX_DIRECTORY, Max_Results, OUTPUT_DIR);
-//        page_bl.RankDocWithBigram_Laplace(pageMap, OUTPUT_DIR+"/"+"BigramLanguageModel-Laplace-Page.run");
-//        BL section_bl = new BL(INDEX_DIRECTORY, Max_Results, OUTPUT_DIR);
-//        section_bl.RankDocWithBigram_Laplace(sectionMap, OUTPUT_DIR+"/"+"BigramLanguageModel-Laplace-Section.run");
+        System.out.println("Running Biagram Language Model with Laplace Smoothing...");
+        BL page_bl = new BL(INDEX_DIRECTORY, Max_Results, OUTPUT_DIR);
+        page_bl.RankDocWithBigram_Laplace(pageMap, OUTPUT_DIR+"/"+"BigramLanguageModel-Laplace-Page.run");
+        BL section_bl = new BL(INDEX_DIRECTORY, Max_Results, OUTPUT_DIR);
+        section_bl.RankDocWithBigram_Laplace(sectionMap, OUTPUT_DIR+"/"+"BigramLanguageModel-Laplace-Section.run");
 
         System.out.println("Finished");
     }
